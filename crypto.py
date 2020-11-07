1d950c27fb5c176e97051930760ddff41e4fc14e
triciaz <70544111+triciaz@users.noreply.github.com>
2020-11-06 22:11:35 -0800

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -1,3 +1,4 @@
+import random
 # Caesar Cipher
 # Arguments: string, integer
 # Returns: string
@@ -5,10 +6,10 @@ def encrypt_caesar(plaintext, offset):
     plaintext = plaintext.upper()
     alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     result = " "
-    
+
     for i in plaintext:
         if i in alphabet:
-            i_index = (alphabet.find(letter) + offset) % len(alphabet)
+            i_index = (alphabet.find(i) + offset) % len(alphabet)
             result = result + alphabet[i_index]
         else:
             result = result + i
@@ -20,10 +21,10 @@ def decrypt_caesar(ciphertext, offset):
     ciphertext = ciphertext.upper()
     alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     result = " "
-    
+
     for letter in ciphertext:
        if letter in alphabet:
-           letter_index = (alphabet.find(letter) - offest) % len(alphabet)
+           letter_index = (alphabet.find(letter) - offset) % len(alphabet)
            result = result + alphabet[letter_index]
        else:
            result = result + letter
@@ -33,32 +34,22 @@ def decrypt_caesar(ciphertext, offset):
 # Arguments: string, string
 # Returns: string
 def encrypt_vigenere(plaintext, keyword):
+    cipher = ""
+    for (plain_index, plain_character) in enumerate(plaintext):
+        key_index = plain_index % len(keyword)
+        key_character = keyword[key_index]
+        cipher_character = encrypt_character(plain_character, key_character)
+        cipher += cipher_character
+    return cipher
+
+def encrypt_character(plaintext, keyword):
     alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    result = " "
-    
-    plaintext_length = len(plaintext)
-    exp_keyword = keyword
-    exp_keywordLength = len(exp_keyword)
-
-    while exp_keywordLength < plaintext_length:
-        exp_keyword += keyword
-        exp_keywordLength = len(exp_keyword)
-    keywordPosition = 0
-
-    for letter in plaintext:
-        if letter in alphabet:
-            letter_index = alphabet.find(letter)
-            keyword_char = exp_keyword[keywordPosition]
-            keyword_char_index = alphabet.find(keyword_char)
-            keywordPosition += 1
-            newIndex = letter_index + keyword_char_index
-            if newIndex > 26:
-                newIndex = newIndex - 26
-            new_char = alphabet[newIndex]
-            result += letter
-        else: 
-            result += letter
-    return result
+    character_count = len(alphabet)
+    key_code = alphabet.index(keyword)
+    plain_code = alphabet.index(plaintext)
+    cipher_code = (key_code + plain_code) % character_count
+    cipher = alphabet[cipher_code]
+    return cipher
 
 # Arguments: string, string
 # Returns: string
@@ -89,7 +80,6 @@ def decrypt_vigenere(ciphertext, keyword):
                 result = result + new_char
             else:
                 result = result + letter
-
     return result
 
 #  Merkle-Hellman Knapsack Cryptosystem
@@ -102,73 +92,102 @@ def generate_private_key(n=8):
         total = sum(w[:])
         z = random.randint(total + 1, 2 * total)
         w.append(z)
-    assert utils.is_superincreasing(w) == True
+    assert is_superincreasing(w) == True
     w = tuple(w)
 
     total = sum(w[:])
     q = random.randint(total + 1, 2 * total)
 
     for i in range(2, q-1):
-        if utils.coprime(i, q) == True:
+        if is_coprime(i, q) == True:
             r = i
             break
     return (w, q, r)
 
+def is_superincreasing(w):
+    sum = 0
+    for i in w:
+        if i < sum or i == sum:
+            return False
+        sum = sum + i
+    return True
+
+def is_coprime(i, q):
+    if gcd(i, q) == 1:
+        return True
+    return False
+
+def gcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a % b)
+
 # Arguments: tuple (W, Q, R) - W a length-n tuple of integers, Q and R both integers
 # Returns: B - a length-n tuple of integers
 def create_public_key(private_key):
+    w, q, r = private_key
     B = ()
-    for i in range(1, 8):
+    for i in range(len(w)):
         B.append((r * w[i]) % q)
     return B
+
+def extended_gcd(a, b):
+    last_remainder, remainder = abs(a), abs(b)
+    x, last_x, y, last_y = 0, 1, 1, 0
+    while remainder:
+        last_remainder, (quotient, remainder) = remainder, divmod(last_remainder, remainder)
+        x, last_x = last_x - quotient*x, x
+        y, last_y = last_y - quotient*y, y
+    return last_remainder, last_x * (-1 if a < 0 else 1), last_y * (-1 if b < 0 else 1)
+
+def modinv(r, q):
+	g, x, y = extended_gcd(r, q)
+	if g != 1:
+		raise ValueError
+	return x % q
 
 # Arguments: string, tuple B
 # Returns: list of integers
 def encrypt_mhkc(plaintext, public_key):
     C_vals = []
-    res = ''.join(format(ord(i), 'b') for i in plaintext)
-    M = array.array('m',(0 for m in range(0,8))
-
-    C = 0
-    for i in range(1, 8):
-        M[i] = res[i]
-        M = utils.byte_to_bits(i)
-        C_i = M[i] + public_key[i]
-        C = C + C_i
+    byte_arrays = [format(ord(i), '08b') for i in plaintext]
+    for i in byte_arrays:
+        C = 0
+        for j in range(8):
+            if i[j] == '1':
+                C = C + public_key[j]
         C_vals.append(C)
     return C_vals
-    
+
 # Arguments: list of integers, private key (W, Q, R) with W a tuple.
 # Returns: bytearray or str of plaintext
 def decrypt_mhkc(ciphertext, private_key):
-    for i in range(q-1):
+    w, q, r = private_key
+    r_prime = modinv(r, q)
+    a = ""
+    for j in ciphertext:
         s = 0
-        while r*s%q != 1:
-            s = s + 1
-    for i in len(C_vals):
-        C_i = C[i] * s % q
-
-    indices_array = []
-    w_i = 0
-    while s != 0:
-        for x in range(len(w), 1, -1):
-            if w[x] > s:
-                x = x + 1
+        c_prime = (j * r_prime) % q
+        for i in reversed(w):
+            if i < c_prime or i == c_prime:
+                c_prime = c_prime - i
+                s = s*2 + 1
             else:
-                w_i = w[x]
-            indices_array.append(i)
-        s = s - w_i
-        
-    res = ''.join(format(ord(i), '08b') for i in ciphertext)
-    decrypt = []
-    for i in range(0, len(res), 8):
-        ans.append(chr(int(res[i:i+8],2)))
-    ''.join(decrypt)
+                s = s*2
+        print(s)
+        a = a + chr(s)
+    return a
 
-    return decrypt
-    
+def main():
+    print(encrypt_caesar('B', 25))
+    result = encrypt_caesar('B', 25)
+    print(decrypt_caesar(result, 25))
+    print(encrypt_vigenere("IMHIT", "H"))
+    vignere_result = encrypt_vigenere("IMHIT", "H")
+    print(decrypt_vigenere("IMHIT", "H"))
+    print(encrypt_mhkc("POTATOSALAD", (18, 28, 54, 136, 254, 740, 1714, 3194)))
+    print(decrypt_mhkc([164, 5930, 904, 3222, 904, 5930, 5072, 3222, 1022, 3222, 768], ((9, 14, 27, 68, 127, 370, 857, 1597), 4745, 2)))
+
 if __name__ == "__main__":
     main()
-
-
-
